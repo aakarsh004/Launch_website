7cb0b1aa697d41d98d9afefb31aedaf0c53bd96b
aakarsh004 <coderaakarsh@gmail.com>
2026-02-18 22:39:33 +0530

    your message

--- crochetstore/settings.py
+++ crochetstore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gk%vvvs1dx50dm=y&hc0nfjau+h2qhzqkvfd#+ew8w_#_-x$c-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.onrender.com',   # Allows your-app.onrender.com and subdomains
